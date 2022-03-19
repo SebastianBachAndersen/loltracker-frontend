75cb53883bd15f96d399535768fd44c0bcc99d35
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-20 00:28:57 +0100

    change default site name

--- components/meta.js
+++ components/meta.js
@@ -9,7 +9,7 @@ const Meta = ({ title, desc, image }) => {
 
       <meta name="og:title" property="og:title" content={title} />
       <meta name="og:description" property="og:description" content={desc} />
-      <meta property="og:site_name" content="Bogspiloppen" />
+      <meta property="og:site_name" content="Lol Tracker" />
       <meta name="og:type" content="website" />
       {image ? (
         <meta property="og:image" content={`${image}`} />
