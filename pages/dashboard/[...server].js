bc8fb4a83b7604619670bc468c707ce5d1ac9f2c
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-19 21:47:47 +0100

    Serverside render page and meta tags

--- pages/dashboard/[...server].js
+++ pages/dashboard/[...server].js
@@ -6,88 +6,70 @@ import MatchHistoryList from "../../components/matchHistroyList";
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
+import Meta from "../../components/meta";
 
-export default function Dashboard() {
-  const [summoner, setSummoner] = useState([]);
-  const [matchHistory, setMatchHistory] = useState([]);
-  const [loaded, setLoaded] = useState();
-  const [error, setError] = useState();
-  const serverRef = useRef(false);
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+  const { server } = context.query;
+  const res = await fetch(
+    process.env.BASE_API_URL + `summoner/${server[0]}/${server[1]}`
+  );
+  const data = await res.json();
+  data.server = server[0];
+  // Pass data to the page via props
+  return { props: { data } };
+}
 
-  const router = useRouter();
-  const { server } = router.query;
-
-  useEffect(() => {
-    if (!server) return;
-
-    if (serverRef.current === server) return;
-    axios
-      .get(process.env.BASE_API_URL + `summoner/${server[0]}/${server[1]}`)
-      .then((response) => {
-        setSummoner(response.data.summoner);
-        setMatchHistory(response.data.matchHistory);
-        serverRef.current = server;
-        setLoaded(true);
-      })
-      .catch(() => {
-        setError(true);
-      });
-  }, [server, serverRef, summoner, loaded]);
-
+function Dashboard({ data }) {
   return (
     <div>
-      <Head>
-        <title>PLACEHOLDER - league tracker </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      {error ? (
-        <p className="text-red-600"></p>
-      ) : serverRef.current === server ? (
-        <div className="container mx-auto">
-          <div className="grid grid-cols-2 p-5">
-            <div>
-              <h1 className="text-xl">{summoner?.name ?? ""}</h1>
-            </div>
-            <div>
-              <UserSearch
-                selectRegion={"ml-44"}
-                inputInder={
-                  "bg-primary rounded-lg w-96 h-10 text-xl text-slate-50 p-4"
-                }
-                searchButtonOuter={"ml-32 mt-0.5"}
-                searchButtonInder={
-                  "bg-blue-500 hover:bg-blue-700 px-1 py-1 text-white font-bold  rounded"
-                }
-              />
-            </div>
+      <Meta
+        title={`${data.summoner.name} - Lol Tracker - Ranked`}
+        desc={`${data.summoner.name}`}
+        image={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${data.summoner.profileIconId}.jpg`}
+      />
+      <p className="text-red-600"></p>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 p-5">
+          <div>
+            <h1 className="text-xl">{data.summoner.name}</h1>
           </div>
+          <div>
+            <UserSearch
+              selectRegion={"ml-44"}
+              inputInder={
+                "bg-primary rounded-lg w-96 h-10 text-xl text-slate-50 p-4"
+              }
+              searchButtonOuter={"ml-32 mt-0.5"}
+              searchButtonInder={
+                "bg-blue-500 hover:bg-blue-700 px-1 py-1 text-white font-bold  rounded"
+              }
+            />
+          </div>
+        </div>
 
-          <div className="grid grid-cols-4 gap-5">
-            <div>
-              <div className="grid grid-rows-2 gap-5">
-                <div className="bg-primary rounded-md">
-                  <LpGraf />
-                </div>
-                <div className="bg-primary rounded-md">
-                  <FaveChampList />
-                </div>
+        <div className="grid grid-cols-4 gap-5">
+          <div>
+            <div className="grid grid-rows-2 gap-5">
+              <div className="bg-primary rounded-md">
+                <LpGraf />
+              </div>
+              <div className="bg-primary rounded-md">
+                <FaveChampList />
               </div>
             </div>
-            <div className="bg-primary  col-span-3 rounded-md">
-              <MatchHistoryList
-                matchHistory={matchHistory}
-                summoner={summoner}
-                server={server[0]}
-              />
-            </div>
+          </div>
+          <div className="bg-primary  col-span-3 rounded-md">
+            <MatchHistoryList
+              matchHistory={data.matchHistory}
+              summoner={data.summoner}
+              server={data.server}
+            />
           </div>
         </div>
-      ) : (
-        <div className=" flex justify-center items-center">
-          <p className="mx-auto">loading......</p>
-        </div>
-      )}
-      z
+      </div>
     </div>
   );
 }
+
+export default Dashboard;
