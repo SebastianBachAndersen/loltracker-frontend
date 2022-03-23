b2046a84782dbef8489f48545a126a9c37430183
Sebastian Bach Andersen <sban.skp@edu.mercantec.dk>
2022-03-23 23:30:53 +0100

    color

--- components/lpGraf.js
+++ components/lpGraf.js
@@ -24,7 +24,11 @@ const tierList = [
 
 export default function LpGraf({ Data }) {
   const rank = [
-    { date: "Oct 27, 2009", league_points: 0, rank: "iron" }
+    {
+      date: "Mar 15, 2022",
+      league_points: 1306,
+      rank: "gold"
+    }
   ];
   Data?.map((x) => {
     let tier = "iron";
@@ -40,20 +44,57 @@ export default function LpGraf({ Data }) {
     };
     rank.push(lOl_rank);
   });
-  const rank1 = rank.slice(0, rank.length)
-  const rank2 = rank.slice(-rank.length)
-  const percentage = (400/3200) * 100;
-  const ranks = {rank1: rank1, rank2: rank2}
-  console.log(ranks);
+  console.log(rank);
+  const percentage = (400 / 3200) * 100;
+
+  function between(x, min, max) {
+    return x >= min && x <= max;
+  }
+
+  var arr = {};
+  const getColor = () => {
+    rank.map((entry) => {
+      var league = tierList.find((tier) =>
+        between(
+          entry.league_points,
+          tier.league_points,
+          tier.league_points + 399
+        )
+      );
+      if (arr[[league.rank]]) {
+        arr[[league.rank]].push(entry);
+      } else {
+        arr[[league.rank]] = [entry];
+      }
+    });
+    console.log(arr);
+    const getStops = (ranks) => {
+      for (const [key, value] of Object.entries(ranks)) {
+        return (
+          <>
+            <stop offset="0%" stopColor="blue" />
+            <stop offset={`${percentage}%`} stopColor="#9c5221" />
+          </>
+        );
+      }
+    };
+    return (
+      <linearGradient id="gradient" x1="0" y1="0" x2="100%" y2="0">
+        {getStops(arr)}
+      </linearGradient>
+    );
+  };
+
   return (
     <div className="p-4">
+      {getColor()}
       <h2 className="text-center">-current_rank-</h2>
       <div className="bg-sky-50">
         <ResponsiveContainer width="100%" aspect={3}>
           <AreaChart
             width={500}
             height={300}
-            data={ranks}
+            data={rank}
             margin={{
               top: 15,
               right: 30,
@@ -62,28 +103,28 @@ export default function LpGraf({ Data }) {
             }}
           >
             <defs>
-            <linearGradient id="gradient" x1="0" y1="0" x2="100%" y2="0">
-              <stop offset="0%" stopColor="#4d504b" />
-              <stop offset={`${percentage}%`} stopColor="#9c5221" />
-              <stop offset={`${percentage}%`} stopColor="#c2bdb0" />
-              <stop offset={`${percentage}%`} stopColor="#cccc33" />
-              <stop offset={`${percentage}%`} stopColor="#b8b7b2" />
-              <stop offset={`${percentage}%`} stopColor="#cfe4ee" />
-              <stop offset={`${percentage}%`} stopColor="#2272b5" />
-              <stop offset={`${percentage}%`} stopColor="#8884d8" />
-              <stop offset={`100%`} stopColor="#fff" />
-            </linearGradient>
-            <linearGradient id="number2" x1="0" y1="0" x2="100%" y2="0">
-              <stop offset="0%" stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`${percentage}%`} stopColor="blue" />
-              <stop offset={`100%`} stopColor="blue" />
-            </linearGradient>
+              <linearGradient id="gradient" x1="0" y1="0" x2="100%" y2="0">
+                <stop offset="0%" stopColor="red" />
+                <stop offset={`${percentage}%`} stopColor="#9c5221" />
+                <stop offset={`${percentage}%`} stopColor="#c2bdb0" />
+                <stop offset={`${percentage}%`} stopColor="#cccc33" />
+                <stop offset={`${percentage}%`} stopColor="#b8b7b2" />
+                <stop offset={`${percentage}%`} stopColor="#cfe4ee" />
+                <stop offset={`${percentage}%`} stopColor="#2272b5" />
+                <stop offset={`${percentage}%`} stopColor="#8884d8" />
+                <stop offset={`100%`} stopColor="#fff" />
+              </linearGradient>
+              <linearGradient id="number2" x1="0" y1="0" x2="100%" y2="0">
+                <stop offset="0%" stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`${percentage}%`} stopColor="blue" />
+                <stop offset={`100%`} stopColor="blue" />
+              </linearGradient>
             </defs>
             <CartesianGrid
               horizontal="true"
@@ -92,7 +133,10 @@ export default function LpGraf({ Data }) {
               stroke="#243240"
             />
             <XAxis dataKey={"date"} tick={{ fill: "#000" }} />
-            <YAxis tick={{ fill: "#000" }} />
+            <YAxis
+              tick={{ fill: "#000" }}
+              domain={["dataMin", "dataMax + 1"]}
+            />
             <Tooltip
               contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
               itemStyle={{ color: "#fff" }}
@@ -100,16 +144,9 @@ export default function LpGraf({ Data }) {
             />
             <Area
               type="monotone"
-              dataKey="rank1.league_points"
+              dataKey="league_points"
               fill="url(#gradient)"
               stroke="url(#gradient)"
-              strokeWidth="2"
-            />
-            <Area
-              type="monotone"
-              dataKey="rank2.league_points"
-              fill="url(#number2)"
-              stroke="url(#number2)"
               strokeWidth="2"
             />
           </AreaChart>
